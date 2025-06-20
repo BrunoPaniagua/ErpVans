e1f75e02d820c1501bdbf685e03bc57bc767807d
Bruno Paniagua <brunopaniaguaoliveira@gmail.com>
2025-06-20 17:55:47 -0300

    criando e configurando o projeto

---
+++ src/main/java/br/com/erpvan/ErpvanApplication.java
@@ -0,0 +1,13 @@
+package br.com.erpvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erpvan/ErpvanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erpvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
